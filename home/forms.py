e3c50a16d607472eb6a0fb6380896ea22f75b9c8
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2022-01-16 04:28:15 +0500

    Delete forms.py

--- home/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import fields, ModelForm
-from home.models import BugTickets
-
-class BugTicketsForm(ModelForm):
-    class Meta:
-        model = BugTickets
-        fields = "__all__"
