427828c5a4ee648e7f72764d418ea8bd2e939489
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2022-01-16 04:39:18 +0500

    real one

--- Bug_Tracker/BugTracker/BugTracker/settings.py
+++ Bug_Tracker/BugTracker/BugTracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e35=@hb66%1@wyiga1sit5&(3-*g*z%lc8lh+f)him!qdw-(i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bug---tracker.herokuapp.com']
 
 
 # Application definition
@@ -127,13 +128,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/teams/'
 
 #Added Manually by Harry Bhai
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static"
 ]
+
+
+# IF DEBUG=True xD
+# if DEBUG == False: 
+#     STATIC_ROOT = ''
+#     STATICFILES_DIRS = ( os.path.join('static'), )
+
+
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/'),
+# ]
 
 MEDIA_ROOT = BASE_DIR / 'static/img/teams'
 
