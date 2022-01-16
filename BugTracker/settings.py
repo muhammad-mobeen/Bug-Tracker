4e753c19e84e98352e759a571c4e02b404612dd1
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2022-01-16 13:32:24 +0500

    Update settings.py

--- BugTracker/settings.py
+++ BugTracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e35=@hb66%1@wyiga1sit5&(3-*g*z%lc8lh+f)him!qdw-(i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bug---tracker.herokuapp.com']
 
