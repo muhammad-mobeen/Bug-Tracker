687c17854796b70d2d88ab7f1f93523e7c79da7f
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2022-01-16 05:07:39 +0500

    Update settings.py

--- BugTracker/settings.py
+++ BugTracker/settings.py
@@ -45,6 +45,7 @@ AUTH_USER_MODEL = 'home.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
